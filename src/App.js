07f8880e9ee2b9493b2a683b0e21f1716f1f992e
arsalansheikh171 <arsalan@rcdppk.org>
2021-01-13 19:02:00 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import comA from "./ComA";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ComA />
+    </div>
+  );
+}
+
+export default App;
+
